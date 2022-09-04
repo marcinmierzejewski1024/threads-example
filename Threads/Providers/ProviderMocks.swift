18169f1e2cab60a11cb1e0aac62a81d9073e4a42
Marcin Mierzejewski <marcinmierzejewski1024@gmail.com>
2022-09-04 17:57:43 +0200

    fix mock configurator

--- Threads/Providers/ProviderMocks.swift
+++ Threads/Providers/ProviderMocks.swift
@@ -44,19 +44,29 @@ class LogUploaderMockImpl : LogUploader {
 class ThreadViewModelMockConfiguratorImpl : ThreadViewModelConfigurator {
     func prepareThreads(t1Interval: TimeInterval, t2Interval: TimeInterval, queueSize: Int, url: String) -> (T1, T2, T3) {
         
+        let sharedListService = SemaphoreSharedListService()
+        let packagesQueueService = SemaphoreSharedListService()
+
+        
         let t1 = T1()
         t1.interval = t1Interval
         t1.batteryProvider = BatteryMockProviderImpl()
+        t1.sharedListService = sharedListService
         
         let t2 = T2()
         t2.interval = t2Interval
         t2.locationProvider = LocationMockProviderImpl()
+        t2.sharedListService = sharedListService
+
         
         let t3 = T3()
         t3.queueSize = queueSize
         t3.url = URL(string: url)
         t3.interval = min(t1Interval, t2Interval) * 0.1
         t3.uploader = LogUploaderMockImpl()
+        t3.sharedListService = sharedListService
+        t3.packagesQueueService = packagesQueueService
+
         
         return (t1,t2,t3)
     }
